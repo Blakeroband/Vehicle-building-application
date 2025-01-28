de5b058ce0c7753c3f1a66bf065c53e4b0183289
B A <blakeroband@gmail.com>
2025-01-28 16:59:08 -0600

    tow is finished, wheelie i think is finished, saving because nothing is broken

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -310,6 +310,8 @@ class Cli {
             'Reverse',
             'Select or create another vehicle',
             'Exit',
+            `Tow`,
+            `Wheelie`,
           ],
         },
       ])
@@ -371,10 +373,24 @@ class Cli {
               this.vehicles[i].reverse();
             }
           }
-        }
-        // TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
+        } else if (answers.action === `Tow`) {
+          const selectedVehicle = this.vehicles.find(vehicle => vehicle.vin === this.selectedVehicleVin);
+          if(selectedVehicle instanceof Truck) {
+            this.findVehicleToTow(selectedVehicle)
+            return;
+          } else {
+            console.log(`vehicle cannot tow`)
+          }
         // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
-        else if (answers.action === 'Select or create another vehicle') {
+        } else if (answers.action === `Wheelie`){ 
+          const selectedVehicle = this.vehicles.find(vehicle => vehicle.vin === this.selectedVehicleVin);
+          if(selectedVehicle instanceof Motorbike) {
+            console.log(`you popped a wheelie`);
+            return;
+          } else {
+            console.log(`this is not a motorbike`)
+          }
+        }else if (answers.action === 'Select or create another vehicle') {
           // start the cli to return to the initial prompt if the user wants to select or create another vehicle
           this.startCli();
           return;
